ff43e7e6d04dd0a1110db7c95521565b8b684091
Adam Gomez <adam.daniel.gomez.787@gmail.com>
2023-01-03 08:50:29 -0600

    adding lesson notebook and warmup

---
+++ pizza_sql_warmup_1.sql
@@ -0,0 +1,22 @@
+# SQL Warmup
+# Use the pizza database to answer the following questions.
+
+# What information is stored in the toppings table? How does this table relate to the pizzas table?
+
+# What information is stored in the modifiers table? How does this table relate to the pizzas table?
+
+# How are the pizzas and sizes tables related?
+
+# What other tables are in the database?
+
+# How many unique toppings are there?
+
+# How many unique orders are in this dataset?
+
+# Which size of pizza is sold the most?
+
+# How many pizzas have been sold in total?
+
+# What is the most common size of pizza ordered?
+
+# What is the average number of pizzas per order?
